105daba09c9812da1cc3263d2ee9f8394ad41df6
cardu <cardujj@gmail.com>
2025-04-20 22:12:30 -0300

    Add auth protect

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,23 @@
 import express from 'express'
 import path from 'path'
 import router from './router'
+import morgan from 'morgan'
+import cors from 'cors'
+import { protect } from './modules/auth'
 
 const app = express()
 
+app.use(cors())
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 app.use(express.static("static"));
 
 app.get('/', (req, res) => {
-  // res.send('Hello from Express!')
-  // res.json({message: 'Hello from Express'})
   res.status(200)
   res.sendFile(path.resolve("pages/index.html"));
 })
 
-app.use('/api', router)
+app.use('/api', protect, router)
 
 export default app
